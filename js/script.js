961c12a104390f3e1899ac6b00b346f24d9e6138
MuhammadAliy <aliyibnzayir@gmail.com>
2022-12-07 23:21:44 +0500

    fellLove

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector(".btn"),
+    heart = document.querySelector(".heart"),
+    box = document.querySelector(".box");
+// hearts = document.querySelector('.my-heart');
+
+
+btn.addEventListener("click", () => {
+
+    box.classList.toggle("active")
+    heart.classList.add("active")
+
+    heart.style.boxShadow = "1px 1px 15px crimson"
+
+    heart.addEventListener("mouseenter", () => {
+        heart.style.background = "black"
+        heart.style.borderRadius = "5px"
+        heart.style.boxShadow = "5px 5px 20px black"
+
+    })
+    heart.addEventListener("mouseout", () => {
+        heart.style.background = "#fff"
+    })
+})
